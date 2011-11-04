e3a55cbc5f32e69dafff3e0b5b940406d3c14afb
Abdullah Diaa <abdullah.diaa@gmail.com>
2011-11-04 09:43:36 +0200

    match ios and android to be incase sensetive

--- data/js/ua.js
+++ data/js/ua.js
@@ -1,10 +1,12 @@
 $('#ua').keypress(function(e) {
     if(e.keyCode == 13) {
-        self.postMessage(this.value);
+		if(this.value){
+			self.postMessage(this.value);
+		}
     }
 });
 self.on("message", function onMessage(message) {
 	if(message == 'focus'){
-		$('#ua').focus().select();				
+		$('#ua').focus().select();
 	}
 });
