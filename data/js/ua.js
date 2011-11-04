4e709d4f63cf8ea408010b7db14432f7b56225e1
Abdullah Diaa <abdullah.diaa@gmail.com>
2011-11-04 08:49:10 +0200

    [feature] custom UA by pressing shift+U in the browser

---
+++ data/js/ua.js
@@ -0,0 +1,10 @@
+$('#ua').keypress(function(e) {
+    if(e.keyCode == 13) {
+        self.postMessage(this.value);
+    }
+});
+self.on("message", function onMessage(message) {
+	if(message == 'focus'){
+		$('#ua').focus();				
+	}
+});
